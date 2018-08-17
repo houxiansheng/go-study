fcc32a709552a4f240735f63a53edf6453c3e15c
hxb <houxianbiao@xin.com>
2018-08-17 10:05:15 +0800

    hxb:test

--- hello.go
+++ hello.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
-	"time"
+	"test"
 )
 var global int=0;
 func main() {
@@ -11,6 +11,7 @@ func main() {
 	var a = 1.5
 	var b = 2
 	fmt.Println(a,b)
+	test.TestArr();
 
 	//db,err := sql.Open("mysql","root:root@192.168.186.129/sql-collector")
 	//rows, err := db.Query("select * from sql_query")
@@ -19,16 +20,16 @@ func main() {
 	//for rows.Next(){
 	//
 	//}
-	getMysql();
-	go coroutineTest();
-	go coroutineTest();
-	go coroutineTest();
-	go coroutineTest();
-	go coroutineTest();
-	go coroutineTest();
-	go coroutineTest();
-	go coroutineTest();
-	go coroutineTest();
-	fmt.Println(a,a)
-	time.Sleep(time.Duration(15)*time.Second)
+	//getMysql();
+	//go coroutineTest();
+	//go coroutineTest();
+	//go coroutineTest();
+	//go coroutineTest();
+	//go coroutineTest();
+	//go coroutineTest();
+	//go coroutineTest();
+	//go coroutineTest();
+	//go coroutineTest();
+	//fmt.Println(a,a)
+	//time.Sleep(time.Duration(15)*time.Second)
 }
