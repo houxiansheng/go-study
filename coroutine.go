b0075dd881e36b0f3a5ef70ce2c455e15745b375
hxb <houxianbiao@xin.com>
2018-08-24 19:10:16 +0800

    hxb

--- coroutine.go
+++ coroutine.go
@@ -2,13 +2,29 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
 func coroutineTest(){
+	var mutex sync.Mutex;
 	var a int;
 	for a=0; a<10;a++  {
+		mutex.Lock();
 		global++;
-		fmt.Println(global);
+		fmt.Println(a);
+		mutex.Unlock();
 		//time.Sleep(time.Duration(1)*time.Second)
+	}
+}
+func coroutineLock(){
+	var mutex sync.Mutex;
+	var a int;
+	for a=0; a<10;a++  {
+		mutex.Lock();
+		global++;
+		fmt.Println("lock--",a);
+		time.Sleep(time.Duration(1)*time.Second)
+		mutex.Unlock();
 	}
 }
--- hello.go
+++ hello.go
@@ -1,17 +1,32 @@
 package main
 
 import (
-	"fmt"
 	_ "github.com/go-sql-driver/mysql"
-	"test"
+	"cache"
+	"fmt"
+	"time"
 )
 var global int=0;
 func main() {
-	fmt.Println("Hello, World!")
-	var a = 1.5
-	var b = 2
-	fmt.Println(a,b)
-	test.TestArr();
+
+	timestamp := time.Now().Unix()
+
+	fmt.Println(timestamp)
+
+	cache.Instance();
+	var str=cache.Set("a","xcvbnm,.cvbnm,./",1);
+	fmt.Println(str)
+	time.Sleep(time.Duration(3)*time.Second)
+	fmt.Println(cache.Get("a"));
+	go cache.Server();
+	time.Sleep(time.Duration(3)*time.Second)
+	go cache.Client();
+	go cache.Client();
+	time.Sleep(time.Duration(15)*time.Second)
+	//var a = 1.5
+	//var b = 2
+	//fmt.Println(a,b)
+	//test.TestArr();
 
 	//db,err := sql.Open("mysql","root:root@192.168.186.129/sql-collector")
 	//rows, err := db.Query("select * from sql_query")
@@ -21,15 +36,18 @@ func main() {
 	//
 	//}
 	//getMysql();
+	//go coroutineLock();
+	//go coroutineLock();
+	//go coroutineLock();
+	//go coroutineLock();
+	//go coroutineLock();
+	//go coroutineLock();
 	//go coroutineTest();
 	//go coroutineTest();
 	//go coroutineTest();
 	//go coroutineTest();
 	//go coroutineTest();
 	//go coroutineTest();
-	//go coroutineTest();
-	//go coroutineTest();
-	//go coroutineTest();
-	//fmt.Println(a,a)
+	////fmt.Println(a,a)
 	//time.Sleep(time.Duration(15)*time.Second)
 }
